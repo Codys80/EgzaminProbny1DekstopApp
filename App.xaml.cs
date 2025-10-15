6ce8aa7fa4f9197060032158c4a1eaf7978181f0
CodyS80 <bartolosemolo@gmail.com>
2025-10-15 10:16:58 +0200

    Dodanie projektu

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EgzaminProbny1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
